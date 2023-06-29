bcbea6254e6c0eb738a42929d991e4789723d495
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-29 17:51:18 +0545

    minor changes

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const path=require('path')
-const express=require('express')
-const router= express.Router()
-const rootDir=require('..util/path.js')
-
-router.get('/',(res,req)=>{
-    res.sendFile(path.join(rootDir,'views','index.html'))
-})
-module.exports=router
