99f4971aff2d35fe18ed1b102d40466443f1fb45
sushilbhatta <sushilbhatta58@gmail.com>
2023-07-22 02:49:29 +0545

    logut feature added

--- backend/views/dash.js
+++ backend/views/dash.js
@@ -5,3 +5,24 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => {
     nav.classList.toggle("navclose");
 })
+
+
+//logout modal features
+//select all the selectors needed
+const logout=document.querySelector('.logout')
+const modal=document.querySelector('.modal-case')
+const yesBtn=document.querySelector('.yes-btn')
+const noBtn=document.querySelector('.no-btn')
+//when  logout btn is clicked show class 'show' and modal will open.
+logout.addEventListener('click',(event)=>{
+    console.log(event)
+    modal.classList.toggle('show')    
+})
+yesBtn.addEventListener('click',()=>{
+    console.log('You are logged out.')
+//redirect to  the appropriate page.
+})
+noBtn.addEventListener('click',()=>{
+    modal.classList.remove('show')
+})
+
